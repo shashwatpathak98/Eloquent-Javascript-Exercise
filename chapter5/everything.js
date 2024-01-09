bc341ac7900d84a26cf2c515a0798bc6b47e4013
Shashwat Pathak <shashwatpathak18@gmail.com>
2024-01-10 02:52:11 +0530

    feat: added new files

--- chapter5/everything.js
+++ chapter5/everything.js
@@ -1,47 +1,46 @@
 //My Solution
 
 //Using a loop
-const every = (array , test) => {
-    for(let i = 0 ;  i< array.length ; i++){
-       if(!test(array[i])) return false
-    }
-    return true
-}
+const every = (array, test) => {
+  for (let i = 0; i < array.length; i++) {
+    if (!test(array[i])) return false;
+  }
+  return true;
+};
 
-console.log(every([1, 3, 5], n => n < 10 ))
-console.log(every([2, 4, 16], n => n < 10 ))
-console.log(every([], n => n < 10 ))
+console.log(every([1, 3, 5], (n) => n < 10));
+console.log(every([2, 4, 16], (n) => n < 10));
+console.log(every([], (n) => n < 10));
 
 //Using some method
 
-const everyNew = (array , test) =>  !array.some(e => !test(e))
-console.log(everyNew([1, 3, 5], n => n < 10 ))
-console.log(everyNew([2, 4, 16], n => n < 10 ))
-console.log(everyNew([], n => n < 10 ))
-
+const everyNew = (array, test) => !array.some((e) => !test(e));
+console.log(everyNew([1, 3, 5], (n) => n < 10));
+console.log(everyNew([2, 4, 16], (n) => n < 10));
+console.log(everyNew([], (n) => n < 10));
 
 // Author's Solution
 
 function every1(array, predicate) {
-    for (let element of array) {
-      if (!predicate(element)) return false;
-    }
-    return true;
+  for (let element of array) {
+    if (!predicate(element)) return false;
   }
-  
-  function every2(array, predicate) {
-    return !array.some(element => !predicate(element));
-  }
-  
-  console.log(every1([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every1([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every1([], n => n < 10));
-  // → true
-  console.log(every2([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every2([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every2([], n => n < 10));
-  // → true
+  return true;
+}
+
+function every2(array, predicate) {
+  return !array.some((element) => !predicate(element));
+}
+
+console.log(every1([1, 3, 5], (n) => n < 10));
+// → true
+console.log(every1([2, 4, 16], (n) => n < 10));
+// → false
+console.log(every1([], (n) => n < 10));
+// → true
+console.log(every2([1, 3, 5], (n) => n < 10));
+// → true
+console.log(every2([2, 4, 16], (n) => n < 10));
+// → false
+console.log(every2([], (n) => n < 10));
+// → true
